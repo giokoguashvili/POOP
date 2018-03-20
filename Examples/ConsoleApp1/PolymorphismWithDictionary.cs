32bab51344f6dd105fc4a9991cf44decd81a47c6
Giorgi Koguashvili <kogo27@gmail.com>
2018-03-20 16:58:28 +0400

    added polymorphism example with dictionary

---
+++ Examples/ConsoleApp1/PolymorphismWithDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface IMath<T>
+    {
+        T Sum(T a, T b);
+    }
+
+
+    public class Math<T>
+    {
+        public Func<T, T, T> Sum { get; }
+
+        public Math(Func<T, T, T> sum)
+        {
+            Sum = sum;
+        }
+    }
+
+    public static class PolymorphismWithDictionary
+    {
+        public static T Sum<T>(Math<T> dMath, T a, T b)
+        {
+            return dMath.Sum(a, b);
+        }
+        public static void Run()
+        {
+            var dMath = new Math<int>((a, b) => a + b);
+
+            Sum(dMath, 1, 2);
+        }
+    }
+}
